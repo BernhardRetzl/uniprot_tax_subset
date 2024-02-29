d43865320bb1635462f07cad316c0990e3eccb53
Bernhard Retzl <retzl.werbung@gmail.com>
2024-02-29 12:24:59 +0100

    updated

--- taxonomy.py
+++ taxonomy.py
@@ -1,20 +1,27 @@
 from Bio import SeqIO
 import glob
+import re
 
 
-fasta_file = glob.glob('./*.fasta') [0]
+protein_group_file = glob.glob('proteinGroups.txt')[0]
+
+tax_ids = set()
+
+with open(protein_group_file) as f:
+    for line in f:
+        info = line.split('\t')[7]
+        tax_info = re.findall(r'OX=\d+', info)
+        for tax in tax_info:
+            tax_ids.add(tax.split('OX=')[-1])
 
 
-tax_id_list = list()
-with open('./tax_list.txt') as file:
-    for line in file:
-        line = line.strip()
-        tax_id_list.append(line.split('=')[-1])
+fasta_files = glob.glob('./*.fasta')
+out_handle = open('subset.fasta', 'w')
 
+for fasta_file in fasta_files:
+    for sequence in SeqIO.parse(fasta_file, "fasta"):
+        ox = sequence.description.split('OX=')[-1].split(' ')[0]
+        if ox in tax_ids:
+            SeqIO.write(sequence, out_handle, "fasta")
 
-out_handle = open('./subset.fasta', 'w')
-
-for sequence in SeqIO.parse(fasta_file, "fasta"):
-    ox = sequence.description.split('OX=')[-1].split(' ')[0]
-    if ox in tax_id_list:
-        SeqIO.write(sequence, out_handle, "fasta")
+out_handle.close()
